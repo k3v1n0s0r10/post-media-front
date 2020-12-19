08a5f165822939f582b749c91fbfa5f00ff8fc9b
k3v1n0s0r10 <osoriogrisaleskevin@gmail.com>
2020-12-19 11:53:28 -0500

    using node 14 to ci and adding isolatedMOdules to postitem

---
+++ src/components/post-item/post-body/postBody.tsx
@@ -0,0 +1,5 @@
+const postBody: React.FC = () => {
+  return <div></div>;
+};
+
+export default postBody;
---
+++ src/components/post-item/post-header/postHeader.tsx
@@ -0,0 +1,5 @@
+const PostHeader: React.FC = () => {
+  return <div></div>;
+};
+
+export default PostHeader;
